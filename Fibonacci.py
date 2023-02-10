b6a0b7e9da6720820a39f671b0f8746b37c30e29
Kaviyarasu03 <124576080+Kaviyarasu03@users.noreply.github.com>
2023-02-10 06:06:26 +0530

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,18 @@
+n=int(input("enter n value"))
+a=0
+b=1
+Sum=0
+if(n<=0):
+    Print ("give n value greater than zero")
+elif(n==1):
+    Print(a)
+else:
+   Print(a)
+   print(b)
+   i=3
+   while(i<n):
+       sum=a+b
+       Print(sum)
+       a=b
+       b=sum
+       i+=1
